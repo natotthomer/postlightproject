34a6023792b8e0f449c8ed8f0eb684cbcba45507
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-11-17 16:43:17 -0500

    style paginator

--- src/scripts/components/HomePage.js
+++ src/scripts/components/HomePage.js
@@ -14,8 +14,20 @@ export default class HomePage extends React.Component {
         <SearchBar searchHandler={this.props.searchHandler}
           searchValue={this.props.searchValue} />
         <EmployeeList employees={this.props.employees} />
-        <ReactPaginate pageCount={this.props.numPages} onPageChange={this.props.onPageChange} />
+        <div className="employee-list-paginator-container">
+          <ReactPaginate
+            pageCount={this.props.numPages}
+            onPageChange={this.props.onPageChange}
+            pageRangeDisplayed={3}
+            marginPagesDisplayed={1}
+            containerClassName={'employee-list-paginator'}
+            activeClassName={'employee-list-paginator-active'}
+            nextClassName={'employee-list-paginator-page'}
+            previousClassName={'employee-list-paginator-page'}
+            pageClassName={'employee-list-paginator-page'}
+            breakClassName={'employee-list-paginator-break'} />
+          </div>
       </React.Fragment>
-      )
+    )
   }
 }
