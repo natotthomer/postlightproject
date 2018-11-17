c00fc4e9b71d10c3c57ad23ce131495f28c6c724
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-11-17 11:16:37 -0500

    add catch-all url matcher to home view for React routes

--- postlightproject/urls.py
+++ postlightproject/urls.py
@@ -7,10 +7,12 @@ from django.urls import path
 from postlightproject import views
 
 urlpatterns = [
-    re_path(r'^$', views.home, name='home'),
     re_path(r'^api/employees/', include('employee.urls')),
     re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', views.home, name='home'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += re_path(r'.*', views.home),
