3578c25c5f19aeb84c069b565af836a31c5a87cf
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-11-17 21:39:20 -0500

    only show Paginator if numPages > 1

--- src/scripts/components/HomePage.js
+++ src/scripts/components/HomePage.js
@@ -6,6 +6,20 @@ import SearchBar from './SearchBar'
 import EmployeeList from './EmployeeList'
 
 const HomePage = props => {
+  const paginator = props.numPages > 1
+                  ? <ReactPaginate
+                      pageCount={props.numPages}
+                      onPageChange={props.onPageChange}
+                      pageRangeDisplayed={3}
+                      marginPagesDisplayed={1}
+                      containerClassName={'employee-list-paginator'}
+                      activeClassName={'employee-list-paginator-active'}
+                      nextClassName={'employee-list-paginator-page'}
+                      previousClassName={'employee-list-paginator-page'}
+                      pageClassName={'employee-list-paginator-page'}
+                      breakClassName={'employee-list-paginator-break'} />
+                  : null
+  
   return (
     <React.Fragment>
       <Header text={'Browse Directory'} />
@@ -13,18 +27,8 @@ const HomePage = props => {
         searchValue={props.searchValue} />
       <EmployeeList employees={props.employees} />
       <div className="employee-list-paginator-container">
-        <ReactPaginate
-          pageCount={props.numPages}
-          onPageChange={props.onPageChange}
-          pageRangeDisplayed={3}
-          marginPagesDisplayed={1}
-          containerClassName={'employee-list-paginator'}
-          activeClassName={'employee-list-paginator-active'}
-          nextClassName={'employee-list-paginator-page'}
-          previousClassName={'employee-list-paginator-page'}
-          pageClassName={'employee-list-paginator-page'}
-          breakClassName={'employee-list-paginator-break'} />
-        </div>
+        {paginator}
+      </div>
     </React.Fragment>
   )
 }
